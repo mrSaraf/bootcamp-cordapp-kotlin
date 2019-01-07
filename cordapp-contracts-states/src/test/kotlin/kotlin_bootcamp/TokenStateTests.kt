8e4df517a8563c9123bc3c5ece97ef0debbf9fe9
Akshay Saraf <44959777+mrSaraf@users.noreply.github.com>
2019-01-07 15:37:41 +0530

    Update TokenStateTests.kt
    Added import statements

--- cordapp-contracts-states/src/test/kotlin/kotlin_bootcamp/TokenStateTests.kt
+++ cordapp-contracts-states/src/test/kotlin/kotlin_bootcamp/TokenStateTests.kt
@@ -1,5 +1,11 @@
 package kotlin_bootcamp
 
+import net.corda.core.contracts.ContractState
+import net.corda.core.identity.CordaX500Name
+import net.corda.testing.core.TestIdentity
+import org.junit.Test
+import kotlin.test.assertEquals
+
 class TokenStateTests{
 //    val alice = TestIdentity(CordaX500Name("Alice", "", "GB")).party
 //    val bob = TestIdentity(CordaX500Name("Bob", "", "GB")).party
