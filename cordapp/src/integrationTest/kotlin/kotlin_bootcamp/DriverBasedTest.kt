32a79af9dd6a25e0dd932b245a93e580e9f37628
akshay_saraf <akshay_saraf@persistent.com>
2019-01-09 11:36:21 +0530

    Resolved missing import

--- cordapp/src/integrationTest/kotlin/kotlin_bootcamp/DriverBasedTest.kt
+++ cordapp/src/integrationTest/kotlin/kotlin_bootcamp/DriverBasedTest.kt
@@ -1,6 +1,7 @@
 package kotlin_bootcamp
 
 import net.corda.core.identity.CordaX500Name
+import net.corda.core.utilities.getOrThrow
 import net.corda.testing.core.TestIdentity
 import net.corda.testing.driver.DriverDSL
 import net.corda.testing.driver.DriverParameters
